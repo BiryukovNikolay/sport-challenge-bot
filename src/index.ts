08ceaeae2ec19dcc417cc056c64f6475c35d530d
Nikolay <nbiryukov@kameleoon.com>
2024-08-17 18:35:21 +0200

    feat: remove lods

--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,12 @@ dotenv.config();
 // Получаем токен из переменной окружения
 const token: string = process.env.TELEGRAM_TOKEN as string;
 
-console.log('Токен бота:', token);
-
-
 // Создаем бота
 const bot = new TelegramBot(token, { polling: true });
 
-console.log('Бот создан...');
-
 
 // Обрабатываем команду /start
-bot.onText(/\/start/, (msg) => {
-  console.log('Получена команда /start');
-  
+bot.onText(/\/start/, (msg) => {  
   bot.sendMessage(msg.chat.id, "Привет! Я тестовый бот на TypeScript, и я работаю!");
 });
 
@@ -29,4 +22,4 @@ bot.on('message', (msg) => {
   bot.sendMessage(chatId, 'Вы сказали: ' + msg.text);
 });
 
-console.log('Бот запущен...');
+console.log('Бот запущен 2...');
